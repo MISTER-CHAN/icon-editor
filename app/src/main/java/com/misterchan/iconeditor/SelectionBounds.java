7e471a5253a3267d5149c6ba100cf5635825e0c4
Chan Tsz-to <MISTER_CHAN@outlook.com>
2022-05-14 23:08:10 +0800

    Initial commit

--- app/src/main/java/com/misterchan/iconeditor/SelectionBounds.java
+++ app/src/main/java/com/misterchan/iconeditor/SelectionBounds.java
@@ -2,13 +2,4 @@ package com.misterchan.iconeditor;
 
 class SelectionBounds {
     int left, top, right, bottom;
-
-    SelectionBounds() {};
-
-    SelectionBounds(int left, int top, int right, int bottom) {
-        this.left = left;
-        this.top = top;
-        this.right = right;
-        this.bottom = bottom;
-    }
 }
