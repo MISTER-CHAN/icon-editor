58bb9bf58b7a27d4ae59a543de50955e19c45f26
Chan Tsz-to <MISTER_CHAN@outlook.com>
2023-10-31 22:55:10 +0800

    Commit

--- app/src/main/java/com/misterchan/iconeditor/Layers.java
+++ app/src/main/java/com/misterchan/iconeditor/Layers.java
@@ -181,23 +181,24 @@ public class Layers {
                     final boolean hasExtra = layer == specifiedLayer && extraDst != null;
                     final Bitmap bm = hasFilter || hasExtra ? Bitmap.createBitmap(intW, intH, Bitmap.Config.ARGB_8888) : null;
                     final Canvas cv = bm != null ? new Canvas(bm) : canvas;
-                    final Rect d = bm != null ? intRel : dst;
                     final Paint paint = isSubtreeRoot
                             ? baseBm != null ? BitmapUtils.PAINT_SRC_OVER : BitmapUtils.PAINT_SRC
                             : layer.paint;
+                    if (hasFilter) {
+                        cv.drawBitmap(bitmap, dst, intRel, BitmapUtils.PAINT_SRC);
+                    }
                     try {
-                        if (hasFilter) {
-                            cv.drawBitmap(bitmap, dst, intRel, BitmapUtils.PAINT_SRC);
-                        }
+                        final Rect d = bm != null ? intRel : dst;
+                        final Paint p = hasFilter ? BitmapUtils.PAINT_SRC_OVER : paint;
                         if (layer == specifiedLayer) {
                             if (extraDst != null) {
                                 cv.drawBitmap(specifiedLayerBm, src, intRel, hasFilter ? BitmapUtils.PAINT_SRC_OVER : BitmapUtils.PAINT_SRC);
                                 cv.drawBitmap(extraLayer.getBitmap(), extraSrc, extraDst, BitmapUtils.PAINT_SRC_OVER);
                             } else {
-                                cv.drawBitmap(specifiedLayerBm, src, d, paint);
+                                cv.drawBitmap(specifiedLayerBm, src, d, p);
                             }
                         } else {
-                            cv.drawBitmap(layer.bitmap, src, d, paint);
+                            cv.drawBitmap(layer.bitmap, src, d, p);
                         }
                     } catch (final RuntimeException e) {
                         if (bm != null) bm.recycle();
